5b102df1b02b76509b26e5f1133d2c84df5cd392
Ritesh Tyagi <rtyagi@ptc.com>
2016-06-10 17:50:06 +0530

    Adding question json to database

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/iitserver-dev'
+    uri: 'mongodb://root:root@localhost:27017/iitserver-dev'
   },
 
   seedDB: true
--- server/config/seed.js
+++ server/config/seed.js
@@ -6,12 +6,18 @@
 'use strict';
 // Insert seed models below
 var Thing = require('../api/thing/thing.model');
+var Question = require('../api/question/question.model');
 var User = require('../api/user/user.model');
 
 // Insert seed data below
 var thingSeed = require('../api/thing/thing.seed.json');
 
+var questionSeed = require('../api/question/question.seed.json');
+
 // Insert seed inserts below
 Thing.find({}).remove(function() {
   Thing.create(thingSeed);
+});
+Question.find({}).remove(function(){
+  Question.create(questionSeed);
 });
